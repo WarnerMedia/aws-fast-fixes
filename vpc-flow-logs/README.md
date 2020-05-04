66216469bb5e3b2fe0565450968c5a287b0e582b
aaron caito <acaito@trek10.com>
2020-05-04 12:29:30 -0400

    enable flowlogs for vpcs with ec2 instances

---
+++ vpc-flow-logs/README.md
@@ -0,0 +1,39 @@
+# enable-vpc-flowlogs
+
+This script will enable vpc flow logs in your account.
+
+## Why?
+
+VPC Flow Logs is a feature that enables you to capture information about the IP traffic going to and from network interfaces in your VPC. Flow log data can be published to Amazon CloudWatch Logs or Amazon S3. After you've created a flow log, you can retrieve and view its data in the chosen destination. 
+
+## What the script does.
+
+This script will iterate through all your regions, through all VPCs in each region and enable flow logs for each VPC.
+
+## Usage
+
+```bash
+usage: enable-vpc-flowlogs.py [-h] [--debug] [--error] [--timestamp] [--region REGION] [--profile PROFILE] [--vpc-id VPC_ID] [--actually-do-it] --flowlog-bucket-prefix FLOWLOG_BUCKET_PREFIX
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --debug               print debugging info
+  --error               print error info only
+  --timestamp           Output log with timestamp and toolname
+  --region REGION       Only Process Specified Region
+  --profile PROFILE     Use this CLI profile (instead of default or env credentials)
+  --vpc-id VPC_ID       Only Process Specified VPC
+  --actually-do-it      Actually Perform the action
+  --flowlog-bucket-prefix FLOWLOG_BUCKET_PREFIX S3 bucket to deposit logs to
+```
+
+You must specify `--actually-do-it` for the changes to be made. Otherwise the script runs in dry-run mode only.
+
+
+## AWS Docs
+
+* [VPC Flow Logs](https://docs.aws.amazon.com/vpc/latest/userguide/flow-logs.html)
+* [CreateFlowLogs API](https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_CreateFlowLogs.html)
+* [boto3 create_flow_logs()](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.create_flow_logs)
+
+
---
+++ vpc-flow-logs/enable-vpc-flowlogs.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import concurrent.futures
+
+max_workers = 10
+
+def main(args, logger):
+    '''Executes the Primary Logic'''
+
+    # If they specify a profile use it. Otherwise do the normal thing
+    if args.profile:
+        session = boto3.Session(profile_name=args.profile)
+    else:
+        session = boto3.Session()
+
+    # Get all the Regions for this account
+    all_regions = get_regions(session, args)
+
+    # processiong regions
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for region in all_regions:
+            futures.append(executor.submit(process_region, args, region, session, logger))
+    for future in futures:
+        future.result()
+
+    return
+
+def process_region(args, region, session, logger):
+    logger.debug(f"Processing region {region}")
+    ec2_client = session.client('ec2', region_name=region)
+
+    vpcs = []
+    paginator = ec2_client.get_paginator('describe_vpcs')
+    for page in paginator.paginate():
+        for vpc in page['Vpcs']:
+            if args.vpc_id:
+                if args.vpc_id == vpc['VpcId']:
+                    vpcs.append(vpc['VpcId'])
+            else:
+                vpcs.append(vpc['VpcId'])
+    if vpcs:
+        # checking for existing log bucket
+        s3_client = session.client('s3', region_name=region)
+        bucket_name = '{}-{}'.format(args.flowlog_bucket_prefix,region)
+        try:
+            s3_client.head_bucket(Bucket=bucket_name)
+        except ClientError as e:
+            if e.response['Error']['Code'] == '403':
+                logger.error("Unable access bucket {}: AccessDenied".format(bucket_name))
+            elif e.response['Error']['Code'] == '404':
+                logger.error("Log bucket {} does not exist".format(bucket_name))
+            else:
+                raise
+            return
+
+        # processing VPCs
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for VpcId in vpcs:
+                logger.debug(f"   Processing VpcId {VpcId}")
+                futures.append(executor.submit(enable_flowlogs, VpcId, ec2_client, args, region))
+
+        for future in futures:
+            future.result()
+    else:
+        logger.debug("No VPCs to enable flow logs in region:{}".format(region))
+
+    return
+
+def enable_flowlogs(VpcId,client,args,region):
+    # checking for existing flow logs
+    bucket = 'arn:aws:s3:::{}-{}'.format(args.flowlog_bucket_prefix,region)
+    flow_logs_exist = False
+    paginator = client.get_paginator('describe_flow_logs')
+    for page in paginator.paginate(
+            Filters=[
+                {
+                    'Name': 'resource-id',
+                    'Values': [VpcId]
+                },
+                {
+                    'Name': 'log-destination-type',
+                    'Values': ['s3']
+                }
+            ]
+        ):
+        for FlowLog in page['FlowLogs']:
+            if FlowLog['LogDestination'] == bucket:
+                logger.info("Flow Log ({}) already exist, region:{}, VPC:{}".format(FlowLog['FlowLogId'],region,VpcId))
+                flow_logs_exist = True
+                if FlowLog['DeliverLogsStatus'] == 'FAILED':
+                    logger.error("Flow Log ({}) failed, region:{}, VPC:{}, please check it".format(FlowLog['FlowLogId'],region,VpcId))
+
+    # creating flow logs
+    if args.actually_do_it and not flow_logs_exist:
+        logger.info("enabling Flow Log region:{}, VPC:{}".format(region,VpcId))
+        response = client.create_flow_logs(
+            ResourceIds=[VpcId],
+            ResourceType='VPC',
+            TrafficType='ALL',
+            LogDestinationType='s3',
+            LogDestination=bucket
+        )
+        
+        if response.get('Unsuccessful'):
+            for unsuccess in response['Unsuccessful']:
+                if unsuccess.get('Error'):
+                    logger.error("Flow Log creation failed, error:{}".format(unsuccess['Error'].get('Message')))
+        elif response.get('FlowLogIds'):
+            logger.info("Successfully created Flow Logs:{}, region:{}, VPC:{}".format(response['FlowLogIds'][0],region,VpcId))
+
+    return
+
+def get_regions(session, args):
+    '''Return a list of regions with us-east-1 first. If --region was specified, return a list wth just that'''
+
+    # If we specifed a region on the CLI, return a list of just that
+    if args.region:
+        return([args.region])
+
+    # otherwise return all the regions, us-east-1 first
+    ec2 = session.client('ec2')
+    response = ec2.describe_regions()
+    output = ['us-east-1']
+    for r in response['Regions']:
+        # return us-east-1 first, but dont return it twice
+        if r['RegionName'] == "us-east-1":
+            continue
+        output.append(r['RegionName'])
+    return(output)
+
+
+def do_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="print debugging info", action='store_true')
+    parser.add_argument("--error", help="print error info only", action='store_true')
+    parser.add_argument("--timestamp", help="Output log with timestamp and toolname", action='store_true')
+    parser.add_argument("--region", help="Only Process Specified Region")
+    parser.add_argument("--profile", help="Use this CLI profile (instead of default or env credentials)")
+    parser.add_argument("--vpc-id", help="Only Process Specified VPC")
+    parser.add_argument("--actually-do-it", help="Actually Perform the action", action='store_true')
+    parser.add_argument("--flowlog-bucket-prefix", help="S3 bucket to deposit logs to", required=True)
+
+    args = parser.parse_args()
+
+    return(args)
+
+if __name__ == '__main__':
+
+    args = do_args()
+
+    # Logging idea stolen from: https://docs.python.org/3/howto/logging.html#configuring-logging
+    # create console handler and set level to debug
+    logger = logging.getLogger('enable-vpc-flowlogs')
+    ch = logging.StreamHandler()
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    elif args.error:
+        logger.setLevel(logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # Silence Boto3 & Friends
+    logging.getLogger('botocore').setLevel(logging.WARNING)
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+    # create formatter
+    if args.timestamp:
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    else:
+        formatter = logging.Formatter('%(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+
+    try:
+        main(args, logger)
+    except KeyboardInterrupt:
+        exit(1)
