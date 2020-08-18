8a670340183bb29eb04de77784b52b1baa1733aa
Roshan Bangera <roshan.bangera@turner.com>
2020-08-18 16:45:02 -0400

    * add new arg: boto-region to specify region boto3 client connects to start the session.  * skip exists check of args.profile when creating aws session object.

--- delete-default-vpc/delete-default-vpcs.py
+++ delete-default-vpc/delete-default-vpcs.py
@@ -9,11 +9,7 @@ max_workers = 10
 def main(args, logger):
     '''Executes the Primary Logic'''
 
-    # If they specify a profile use it. Otherwise do the normal thing
-    if args.profile:
-        session = boto3.Session(profile_name=args.profile)
-    else:
-        session = boto3.Session()
+    session = boto3.Session(profile_name=args.profile, region_name=args.boto_region)
 
     # Get all the Regions for this account
     all_regions = get_regions(session, args)
@@ -215,6 +211,7 @@ def do_args():
     parser.add_argument("--timestamp", help="Output log with timestamp and toolname", action='store_true')
     parser.add_argument("--profile", help="Use this CLI profile (instead of default or env credentials)")
     parser.add_argument("--region", help="Only look for default VPCs in this region")
+    parser.add_argument("--boto-region", help="Initial AWS region for boto3 client", default="us-east-1")
     parser.add_argument("--exclude-regions", nargs='+', help="REGION1, REGION2 Do not attempt to delete default VPCs in these regions")
     parser.add_argument("--vpc-id", help="Only delete the VPC specified")
     parser.add_argument("--actually-do-it", help="Actually Perform the action (default behavior is to report on what would be done)", action='store_true')
